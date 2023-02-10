2d31ce82297f8b1049ca5bdffa5d2914e3daf2f7
Kiev Mendoza <kiev.mendoza@dharbor.com>
2023-02-10 02:21:11 -0400

    bootstrap added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabase: {
+    publicKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5dWxjb2JjcG14dHNkZnpsdXNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzYwMDQzMzEsImV4cCI6MTk5MTU4MDMzMX0.En-kSNabbu3zFGnWBEFeoXZSgfxOU4ssLMrqFr9_PmM',
+    url: 'https://vyulcobcpmxtsdfzluss.supabase.co'
+  }
 };
 
 /*
