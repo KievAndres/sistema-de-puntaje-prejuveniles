ab74dd3f78ef009e662b1f6b007fbf1386a83564
Kiev Mendoza <kiev.mendoza@dharbor.com>
2024-10-19 06:44:08 -0400

    construir tabla de responsabilidad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SupabaseService } from '../services/supabase.service';
+import { MiembroEquipo, SubCategoria } from '../types/custom-types';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'puntaje-prejuveniles';
+  public listaMiembros: MiembroEquipo[];
+  public listaSubCategoria: SubCategoria[];
+  constructor(private readonly _supabaseService: SupabaseService) {
+    this.listaMiembros = [];
+    this.listaSubCategoria = [];
+  }
+
+  public async ngOnInit(): Promise<void> {
+    this.listaMiembros = await this._supabaseService.getListaMiembrosRegistroRegla();
+    this.listaSubCategoria = await this._supabaseService.getSubCategoria();
+  }
 }
